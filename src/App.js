b5ad6d95ea4e1fca65aa0d8a727407d47f655979
Joseph Mutua <Mutuaj793@gmail.com>
2022-12-13 16:24:35 +0300

    Finish up Landing PAge

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Routes, Route } from "react-router-dom";
 
 // Components
 import Login from "./pages/Login";
+import LandingPage from "./pages/LandingPage";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Login/>}/>
+        <Route path="/" element={<LandingPage/>}/>
+        <Route path="/login" element={<Login/>}/>
       </Routes>
     </div>
   );
