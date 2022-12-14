3557e8cb9175f1a0f72ba9051dc4c51e1d1d3cae
Joseph Mutua <Mutuaj793@gmail.com>
2022-12-14 12:29:20 +0300

    Add private route

--- src/App.js
+++ src/App.js
@@ -6,17 +6,24 @@ import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 import ResetPassword from "./pages/ResetPassword";
 import Homepage from "./pages/Homepage";
-
-
+import PrivateRoute from "./pages/PrivateRoute";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Login/>}/>
-        <Route path="reset-password" element={<ResetPassword/>}/>
-        <Route path="sign-up" element={<SignUp/>}/>
-        <Route path="homepage"element={<Homepage/>}/>
+        <Route path="/" element={<Login />} />
+        <Route path="reset-password" element={<ResetPassword />} />
+        <Route path="sign-up" element={<SignUp />} />
+
+        <Route
+          path="homepage"
+          element={
+            <PrivateRoute>
+              <Homepage />
+            </PrivateRoute>
+          }
+        />
       </Routes>
     </div>
   );
